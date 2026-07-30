b9771d7f18725f94349572ce1b4fa6922084a0f4
meltedham <yeowraeyen@gmail.com>
2026-07-30 17:52:27 +0800

    fix: remove unused AboutSection import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import NavBar from './components/NavBar'
 import HeroSection from './components/HeroSection'
-import AboutSection from './components/AboutSection'
 import ExperienceSection from './components/ExperienceSection'
 import EducationSection from './components/EducationSection'
 import ProjectsSection from './components/ProjectsSection'
